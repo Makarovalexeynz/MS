45980b47a9be2688cbd569d092a00c74c22b3dda
Aleksei Makarov <makarovalexeynz@gmail.com>
2025-02-24 23:47:11 +0300

    Initial commit

---
+++ licensing-service/src/main/java/com/example/licensing_service/controller/LicenceController.java
@@ -0,0 +1,49 @@
+package com.example.licensing_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.licensing_service.service.LicenseService;
+import com.example.licensing_service.model.License;
+
+@RestController
+@RequestMapping(value="v1/organization/{organizationId}/license")
+public class LicenceController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @GetMapping(value="/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService
+                .getLicense(licenseId,organizationId);
+        return ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId")
+            String organizationId,
+            @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request) {
+        return ResponseEntity.ok(licenseService.createLicense(request,
+                organizationId));
+    }
+
+    @DeleteMapping(value="/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId,
+                organizationId));
+    }
+}
